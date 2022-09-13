8da5b3a3438b6c80c9e0100d3b4e5a65cc7113cb
RingK27 <111756643+RingK27@users.noreply.github.com>
2022-09-13 14:57:27 +0200

    Layout +  basic script
    Added placeholder layout, wrote basic script

--- scripts.js
+++ scripts.js
@@ -1,13 +1,30 @@
-let myLibrary = [];
+let myLibrary = new Array();
+let addBtn = document.getElementById("addBtn");
+let btitle = document.getElementById("btitle");
+let bauth = document.getElementById("bauth");
+let bpages = document.getElementById("bpages");
+let bread = document.getElementById("bread");
+addBtn.addEventListener("click", addBookToLibrary);
 
-function book() {
-
+function book(title, author, pages, isRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
 }
 
 function addBookToLibrary() {
-
+    bookToAdd = new book(btitle.value, bauth.value, bpages.value, bread.value);
+    myLibrary.push(bookToAdd);
+    btitle.value = "";
+    bauth.value = ""; 
+    bpages.value = ""; 
+    bread.value = "";
+    showBooks();
 }
 
 function showBooks() {
-    
+    for (let i = 0; i < myLibrary.length; i++) {
+        console.log(myLibrary[i].title + " " + myLibrary[i].author + " " + myLibrary[i].pages + " " + myLibrary[i].isRead);
+    }  
 }
