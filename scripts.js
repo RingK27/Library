72efb53d898c7fc784c7a142cf6c1713de2255be
RingK27 <111756643+RingK27@users.noreply.github.com>
2022-09-14 17:13:14 +0200

    Fine tuned some things

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ let addBtn = document.getElementById("addBtn");
 let btitle = document.getElementById("btitle");
 let bauth = document.getElementById("bauth");
 let bpages = document.getElementById("bpages");
-let bread = document.getElementById("bread");
+
 addBtn.addEventListener("click", addBookToLibrary);
 
 function book(title, author, pages, isRead) {
@@ -14,7 +14,13 @@ function book(title, author, pages, isRead) {
 }
 
 function addBookToLibrary() {
-    bookToAdd = new book(btitle.value, bauth.value, bpages.value, bread.value);
+    let bread = document.getElementById("bread");
+    if (bread.checked == true) {
+        bread = "yes";
+    } else {
+        bread = "no";
+    }
+    bookToAdd = new book(btitle.value, bauth.value, bpages.value, bread);
     myLibrary.push(bookToAdd);
     btitle.value = "";
     bauth.value = ""; 
@@ -26,17 +32,24 @@ function addBookToLibrary() {
 function showBooks() {
     let container = document.getElementById("book-cards-container");
     container.innerHTML = "";
-    for (let i = 0; i < myLibrary.length; i++) {
-        
+    for (let i = 0; i < myLibrary.length; i++) {        
         const card = document.createElement("div");
         const firstLine = document.createElement("p");
         const secondLine = document.createElement("p");
         const thirdLine = document.createElement("div");
         card.className = "card contains";
         card.setAttribute("style", "padding: 10px")
-        firstLine.innerHTML = myLibrary[i].title + "<br> by " + myLibrary[i].author +"<br><br><br><br>";
-        secondLine.innerHTML = "number of pages: " + myLibrary[i].pages + "<br><br><br>";
-        thirdLine.innerText = "Read: " + myLibrary[i].isRead;
+        firstLine.innerHTML = myLibrary[i].title + "<br> by " 
+            + myLibrary[i].author +"<br><br><br><br>";
+        secondLine.innerHTML = "number of pages: " 
+            + myLibrary[i].pages + "<br><br><br>";
+        if (myLibrary[i].isRead == "yes") {
+            thirdLine.innerHTML = "Read: " 
+                + '<span><img height= 15px src="images/green.png"> YES';
+        } else {
+            thirdLine.innerHTML = "Read: " 
+                + '<span><img height= 15px src="images/red.png"> NO';    
+        }        
         card.appendChild(firstLine);
         card.appendChild(secondLine);
         card.appendChild(thirdLine);
@@ -44,9 +57,12 @@ function showBooks() {
     }  
 }
 
-let leviathan = new book("Leviathan Wakes", "James S. A. Corey", 598, "yes");
-let gradini = new book("Gradini", "Giuseppe Pietro Tornatore", 105, "yes");
-let anelli = new book("Il Signore degli Anelli", "J. R. R. Tolkien", 1380, "yes");
+let leviathan = new book("Leviathan Wakes", 
+    "James S. A. Corey", 598, "yes");
+let gradini = new book("Gradini", 
+    "Giuseppe Pietro Tornatore", 105, "yes");
+let anelli = new book("Il Signore degli Anelli", 
+    "J. R. R. Tolkien", 1380, "yes");
 myLibrary.push(leviathan);
 myLibrary.push(gradini);
 myLibrary.push(anelli);
