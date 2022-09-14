169f460606b43afba614a46bf666d05f4d89e3d0
RingK27 <111756643+RingK27@users.noreply.github.com>
2022-09-14 17:45:16 +0200

    added book buttons

--- scripts.js
+++ scripts.js
@@ -6,11 +6,12 @@ let bpages = document.getElementById("bpages");
 
 addBtn.addEventListener("click", addBookToLibrary);
 
-function book(title, author, pages, isRead) {
+function book(title, author, pages, isRead, iNumber) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.isRead = isRead;
+    this.iNumber = iNumber;
 }
 
 function addBookToLibrary() {
@@ -20,8 +21,9 @@ function addBookToLibrary() {
     } else {
         bread = "no";
     }
-    bookToAdd = new book(btitle.value, bauth.value, bpages.value, bread);
+    bookToAdd = new book(btitle.value, bauth.value, bpages.value, bread, myLibrary.length);
     myLibrary.push(bookToAdd);
+    console.log(bookToAdd.iNumber);
     btitle.value = "";
     bauth.value = ""; 
     bpages.value = ""; 
@@ -37,32 +39,47 @@ function showBooks() {
         const firstLine = document.createElement("p");
         const secondLine = document.createElement("p");
         const thirdLine = document.createElement("div");
+        const fourthLine = document.createElement("div");
+        const btnStatus = document.createElement("button");
+        const btnDel = document.createElement("button");
+        btnStatus.className = "book-btn";
+        btnStatus.innerText = "Change status";                  
+        btnDel.className = "book-btn";
+        btnDel.innerText = "Delete book";        
         card.className = "card contains";
         card.setAttribute("style", "padding: 10px")
         firstLine.innerHTML = myLibrary[i].title + "<br> by " 
-            + myLibrary[i].author +"<br><br><br><br>";
+            + myLibrary[i].author +"<br><br>";
         secondLine.innerHTML = "number of pages: " 
             + myLibrary[i].pages + "<br><br><br>";
         if (myLibrary[i].isRead == "yes") {
             thirdLine.innerHTML = "Read: " 
-                + '<span><img height= 15px src="images/green.png"> YES';
+                + '<span><img height= 15px src="images/green.png"> YES<br><br>';
         } else {
             thirdLine.innerHTML = "Read: " 
-                + '<span><img height= 15px src="images/red.png"> NO';    
-        }        
+                + '<span><img height= 15px src="images/red.png"> NO<br><br>';    
+        }
+        fourthLine.appendChild(btnDel);
+        fourthLine.appendChild(btnStatus);        
         card.appendChild(firstLine);
         card.appendChild(secondLine);
         card.appendChild(thirdLine);
+        card.appendChild(fourthLine);
         container.appendChild(card);
     }  
 }
 
+function delBook(iNumber) {
+    myLibrary.splice(iNumber, 1);
+    showBooks();
+}
+
 let leviathan = new book("Leviathan Wakes", 
-    "James S. A. Corey", 598, "yes");
+    "James S. A. Corey", 598, "yes", 0);
 let gradini = new book("Gradini", 
-    "Giuseppe Pietro Tornatore", 105, "yes");
+    "Giuseppe Pietro Tornatore", 105, "yes", 1);
 let anelli = new book("Il Signore degli Anelli", 
-    "J. R. R. Tolkien", 1380, "yes");
+    "J. R. R. Tolkien", 1380, "yes", 2);
 myLibrary.push(leviathan);
 myLibrary.push(gradini);
 myLibrary.push(anelli);
