aeb608444fbf2d1cc5a0064b72e9fd6cb9a0636c
RingK27 <111756643+RingK27@users.noreply.github.com>
2022-09-16 12:58:23 +0200

    Added all the functions, fixed html

--- scripts.js
+++ scripts.js
@@ -33,44 +33,60 @@ function addBookToLibrary() {
 function showBooks() {
     let container = document.getElementById("book-cards-container");
     container.innerHTML = "";
-    for (let i = 0; i < myLibrary.length; i++) {        
-        const card = document.createElement("div");
-        const firstLine = document.createElement("p");
-        const secondLine = document.createElement("p");
-        const thirdLine = document.createElement("div");
-        const fourthLine = document.createElement("div");
-        const btnStatus = document.createElement("button");
-        const btnDel = document.createElement("button");
-        btnStatus.className = "book-btn";
-        btnStatus.innerText = "Change status";                  
-        btnDel.className = "book-btn";
-        btnDel.innerText = "Delete book";        
-        card.className = "card contains";
-        card.setAttribute("style", "padding: 10px")
-        card.dataset.inumber = myLibrary[i].iNumber;
-        firstLine.innerHTML = myLibrary[i].title + "<br> by " 
-            + myLibrary[i].author +"<br><br>";
-        secondLine.innerHTML = "number of pages: " 
-            + myLibrary[i].pages + "<br><br><br>";
-        if (myLibrary[i].isRead == "yes") {
-            thirdLine.innerHTML = "Read: " 
-                + '<span><img height= 15px src="images/green.png"> YES<br><br>';
-        } else {
-            thirdLine.innerHTML = "Read: " 
-                + '<span><img height= 15px src="images/red.png"> NO<br><br>';    
-        }
-        fourthLine.appendChild(btnDel);
-        fourthLine.appendChild(btnStatus);        
-        card.appendChild(firstLine);
-        card.appendChild(secondLine);
-        card.appendChild(thirdLine);
-        card.appendChild(fourthLine);
-        container.appendChild(card);        
+    for (let i = 0; i < myLibrary.length; i++) { 
+        if (myLibrary[i] != undefined) {
+            const card = document.createElement("div");
+            const firstLine = document.createElement("p");
+            const secondLine = document.createElement("p");
+            const thirdLine = document.createElement("div");
+            const fourthLine = document.createElement("div");
+            const btnStatus = document.createElement("button");
+            const btnDel = document.createElement("button");
+            btnStatus.className = "book-btn";
+            btnStatus.innerText = "Change status";                  
+            btnDel.className = "book-btn";
+            btnDel.innerText = "Delete book";              
+            card.className = "card contains";
+            card.id = "book-" + i;
+            card.setAttribute("style", "padding: 10px")
+            btnDel.dataset.inumber = myLibrary[i].iNumber;
+            btnStatus.dataset.inumber = myLibrary[i].iNumber;
+            firstLine.innerHTML = myLibrary[i].title + "<br> by " 
+                + myLibrary[i].author +"<br><br>";
+            secondLine.innerHTML = "number of pages: " 
+                + myLibrary[i].pages + "<br><br><br>";
+            if (myLibrary[i].isRead == "yes") {
+                thirdLine.innerHTML = "Read: " 
+                    + '<span><img height= 15px src="images/green.png"> YES<br><br>';
+            } else {
+                thirdLine.innerHTML = "Read: " 
+                    + '<span><img height= 15px src="images/red.png"> NO<br><br>';    
+            }
+            btnDel.addEventListener("click", delBook);
+            btnStatus.addEventListener("click", changeStatus);   
+            fourthLine.appendChild(btnDel);
+            fourthLine.appendChild(btnStatus);        
+            card.appendChild(firstLine);
+            card.appendChild(secondLine);
+            card.appendChild(thirdLine);
+            card.appendChild(fourthLine);
+            container.appendChild(card);
+        } 
     }  
 }
 
-function delBook(iNumber) {
-    myLibrary.splice(iNumber, 1);
+function delBook() {    
+    myLibrary[this.dataset.inumber] = null;
+    console.log(myLibrary);
+    showBooks();   
+}
+
+function changeStatus() {
+    if (myLibrary[this.dataset.inumber].isRead == "yes") {
+        myLibrary[this.dataset.inumber].isRead = "no";
+    } else {
+        myLibrary[this.dataset.inumber].isRead = "yes"; 
+    }    
     showBooks();
 }
 
